d66f82df7511e66d1eb0aeab40e7421d78a669cf
anilkharatmol <akharatmol8@gmail.com>
2023-12-22 23:35:28 +0530

    request/response task

--- server.js
+++ server.js
@@ -1,7 +1,46 @@
 const http=require('http');
 
 const server =http.createServer((req,res)=>{
-    console.log('My name is Anil Kharatmol');
+    console.log('My name is Anil Kharatmol',req.url,req.method,req.headers);
+    const url =req.url;
+    const method =req.method;
+
+    if (url==='/') {
+        res.write('<html>')
+        res.write('<header><title>Enter message</title></header>')
+        res.write(`<body><form action= "/message" method="POST"><input type="text"/><button type="submit">Send</button></form></body>`)
+        res.write('<html>')
+        res.write('</html>')
+        return res.end();
+    }
+
+    if (url=== "/home") {
+        res.write('<html>')
+        res.write('<body><h1>Welcome home</h1></body>')
+        res.write('</html>')
+       return res.end();
+    }
+
+    if (url=== "/about") {
+        res.write('<html>')
+        res.write('<body><h1>Welcome to About us page</h1></body>')
+        res.write('</html>')
+       return res.end();
+    }
+
+    if (url=== "/node") {
+        res.write('<html>')
+        res.write('<body><h1>Welcome to my Node Js project</h1></body>')
+        res.write('</html>')
+       return res.end();
+    }
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>')
+    res.write('<header><title>My first node server</title></header>')
+    res.write('<body><h1>Welcome to my node js server</h1></body>')
+    res.write('<html>')
+    res.write('</html>')
+    res.end();
 })
 
 server.listen(4000)
